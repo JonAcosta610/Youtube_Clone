d1d0c534ed786c6324d517edc1575de4249b2f39
Jonathan Acosta <jonathanacosta@Jonathans-MBP.lan>
2023-04-17 21:21:55 -0400

    passed comment box as props to video component + added get requests to get all comments for a video + updated post request to allow user to post comments

--- frontend/src/components/CommentBox/CommentBox.jsx
+++ frontend/src/components/CommentBox/CommentBox.jsx
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './CommentBox.css'
 import axios from 'axios';
+import useAuth from '../../hooks/useAuth';
 
-const CommentBox = () => {
-
+const CommentBox = ({videoId}) => {
   const [comment, setComment] = useState('') 
+  const [comments, setComments] = useState([])
+
+  const [user] = useAuth()
+
+  useEffect(async() => {
+    let response = await axios.get(`http://127.0.0.1:8000/api/comments/${videoId}`
+      )
+      const {items} = response.data
+      setComments(items)
+  },[])
 
   async function postComment(event){
       event.preventDefault()
-      await axios.post('http://127.0.0.1:8000/api/comments/')
+      await axios.post(`http://127.0.0.1:8000/api/comments/`, {
+        "video_id": videoId,
+        "text": comment,
+        "likes": 0,
+        "dislikes": 0
+      })
     }
   
   const handleCommentChange = (event) => {
@@ -17,11 +32,16 @@ const CommentBox = () => {
   
   return (
     <div class='comment-box'>
-      <form>
-        <label>Comments </label>
+      <label>Comments </label>
+      {user && (<form>
         <input type='text' onChange={(event) => handleCommentChange(event)}></input>
         <button onClick={(event) => postComment}>Post Your Comment</button>
-      </form>
+      </form>)}
+      <ul>
+        {comments?.map((comment) => (
+          <li>{comment.text}</li>
+        ))}
+      </ul>
     </div>
   )
 }
