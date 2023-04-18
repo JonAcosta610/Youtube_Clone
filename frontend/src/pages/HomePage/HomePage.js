9d082e966add63fc9754a40dc691ef8a8a1d78a6
Jonathan Acosta <jonathanacosta@Jonathans-MBP.lan>
2023-04-18 11:48:18 -0400

    cleaned up some white space

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import Search from "../../components/Search/Search";
 import { useNavigate, Link, Routes, Route } from "react-router-dom";
 import '../../components/Videos/Videos.css';
-
-
 import axios from "axios";
 
 const HomePage = () => {
@@ -38,7 +36,6 @@ export default HomePage;
 // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
 // The "token" value is the JWT token that you will send in the header of any request requiring authentication
 //TODO: Add an AddCars Page to add a car for a logged in user's garage
-// const [user, token] = useAuth();
 // const [cars, setCars] = useState([]);
 
 // useEffect(() => {
